59ebd01f62dee95110429b77baa4e354a5a34423
NSD <niloysd7@gmail.com>
2024-05-07 22:49:21 +0530

    Finished html

---
+++ index.js
@@ -0,0 +1,14 @@
+async function fetchData() {
+  try {
+    const response = await fetch("data.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const data = await fetchData();
